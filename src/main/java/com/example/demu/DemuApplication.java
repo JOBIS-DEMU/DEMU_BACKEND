aa5cb4fa32e791088500830335b7655e15c75945
RyuJeaMin <kariel7673@gmail.com>
2024-09-20 14:49:07 +0900

    project setting :: 추가

---
+++ src/main/java/com/example/demu/DemuApplication.java
@@ -0,0 +1,13 @@
+package com.example.demu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemuApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demu/domain/user/domain/type/Grade.java
@@ -0,0 +1,9 @@
+package com.example.demu.domain.user.domain.type;
+
+public enum Grade {
+    BRONZE,
+    SILVER,
+    GOLD,
+    PLATINUM,
+    DIAMOND;
+}
