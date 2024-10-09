e1e162373b1459afc3900f6f97ba150ef4135d70
민수아 <Minsua0503@gmail.com>
2024-10-09 19:39:46 +0900

    chore :: import 삭제

--- src/main/java/com/example/demu/global/config/SecurityConfig.java
+++ src/main/java/com/example/demu/global/config/SecurityConfig.java
@@ -1,18 +1,13 @@
 package com.example.demu.global.config;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.SecurityFilterChain;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig extends SecurityConfigurerAdapter {
-
 
 }
