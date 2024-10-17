a6ee65424a3288e4ec2703b363ca0316090a2bd6
RyuJeaMin <kariel7673@gmail.com>
2024-10-17 22:20:18 +0900

    제목으로 검색,카테고리별로 보기 :: 추가

--- src/main/java/com/example/demu/domain/post/domain/repository/PostRepository.java
+++ src/main/java/com/example/demu/domain/post/domain/repository/PostRepository.java
@@ -1,9 +1,14 @@
 package com.example.demu.domain.post.domain.repository;
 
 import com.example.demu.domain.post.domain.Post;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
-public interface PostRepository extends CrudRepository<Post,Long> {
+public interface PostRepository extends JpaRepository<Post,Long> , JpaSpecificationExecutor<Post> {
+
 }
