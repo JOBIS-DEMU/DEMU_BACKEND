d91f7dc7464a6a685dc1fa85ef9227aa98e9d20e
RyuJeaMin <kariel7673@gmail.com>
2024-10-18 15:56:33 +0900

    배포관련 files :: 삭제

--- src/main/java/com/example/demu/domain/comment/controller/CommentController.java
+++
@@ -1,4 +0,0 @@
-package com.example.demu.domain.comment.controller;
-
-public class CommentController {
-}
--- src/main/java/com/example/demu/domain/comment/domain/repository/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demu.domain.comment.domain.repository;
-
-import com.example.demu.domain.comment.domain.Comment;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CommentRepository extends CrudRepository<Comment, Long> {
-}
