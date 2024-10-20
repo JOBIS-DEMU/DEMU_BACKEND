c10344613e5d5c6bb194d1cccd92d210eabd6340
하이 <kkm07133@naver.com>
2024-10-20 17:01:13 +0900

    환경변수 로딩 설정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,7 @@ REPOSITORY=/home/ubuntu/app
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fla java | grep hayan | awk '{print $1}')
+CURRENT_PID=$(pgrep -fla java | grep haya | awk '{print $1}')
 
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
@@ -18,6 +18,14 @@ fi
 
 echo "> 새 애플리케이션 배포"
 
+# S3에서 .env 파일 다운로드
+echo "> S3에서 .env 파일 다운로드"
+aws s3 cp s3://demu-deploy/.env ./.env
+
+# 환경 변수를 로드하기
+export $(cat .env | xargs)
+
+# 최신 JAR 파일 찾기
 JAR_NAME=$(ls -tr $REPOSITORY/*SNAPSHOT.jar | tail -n 1)
 
 echo "> JAR NAME: $JAR_NAME"
