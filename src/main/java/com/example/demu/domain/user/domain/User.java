9494ac0f0d0bfd2b6d9d1ef6f30772acd761e2bd
하이 <kkm07133@naver.com>
2024-10-20 18:36:38 +0900

    BugFix

--- src/main/java/com/example/demu/domain/user/domain/User.java
+++ src/main/java/com/example/demu/domain/user/domain/User.java
@@ -7,13 +7,10 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.ColumnDefault;
-import org.springframework.data.annotation.Id;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
-
 import javax.persistence.*;
 
 @Getter
