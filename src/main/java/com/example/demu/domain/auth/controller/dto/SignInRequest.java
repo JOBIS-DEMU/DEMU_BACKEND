258e8fa0b0f97edbc93a850943a634746789ed0f
RyuJeaMin <kariel7673@gmail.com>
2024-10-24 10:33:21 +0900

    로그인 :: fix

--- src/main/java/com/example/demu/domain/auth/controller/dto/SignInRequest.java
+++ src/main/java/com/example/demu/domain/auth/controller/dto/SignInRequest.java
@@ -1,17 +1,24 @@
 package com.example.demu.domain.auth.controller.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
-@Getter
+
 @NoArgsConstructor
+@Getter
+@Setter
 public class SignInRequest {
 
     @NotBlank
+    @JsonProperty("accountId") // JSON 키와 매핑 보장
     private String accountId;
 
     @NotBlank
     private String password;
+
 }
