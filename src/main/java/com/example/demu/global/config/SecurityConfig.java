d1a0acfcf8c31f06f18ca6adc80dc30cc82cbd09
하이 <kkm07133@naver.com>
2024-10-28 17:30:20 +0900

    url 직접 명시

--- src/main/java/com/example/demu/global/config/SecurityConfig.java
+++ src/main/java/com/example/demu/global/config/SecurityConfig.java
@@ -48,7 +48,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("*"));
+        config.setAllowedOrigins(List.of("http://localhost:5713"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowCredentials(true);
         config.setAllowedHeaders(List.of("*"));
