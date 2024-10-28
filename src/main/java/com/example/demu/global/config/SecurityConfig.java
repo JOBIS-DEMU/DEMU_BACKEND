f4047edcd07aa7bbc2dd3c9afb8b9d71704c7d17
하이 <kkm07133@naver.com>
2024-10-28 22:40:02 +0900

    cors

--- src/main/java/com/example/demu/global/config/SecurityConfig.java
+++ src/main/java/com/example/demu/global/config/SecurityConfig.java
@@ -18,6 +18,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 
+import java.util.Arrays;
 import java.util.List;
 
 @Configuration
@@ -42,23 +43,25 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and()
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
-                // 나머진 인증 필요
+        // 나머진 인증 필요
     }
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5713", "http://3.37.219.136:8080", "http://localhost:3000"));
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        config.setAllowCredentials(true);
-        config.setAllowedHeaders(List.of("*"));
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOrigins(List.of("*")); // 모든 도메인 허용
+        configuration.setAllowedMethods(Arrays.asList("OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE")); // HTTP 메서드 허용
+        configuration.setAllowCredentials(false);
+        configuration.addAllowedHeader("*"); // 모든 헤더 허용
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**",config);
+        source.registerCorsConfiguration("/**", configuration); // 모든 경로에 대해 위에서 설정한 CORS 설정 적용
         return source;
     }
 
-    @Bean // bean으로 등록 해줌으로서 다른클래스에서 의존성을 주입받아 사용할 수 있음.
+    @Bean
+        // bean으로 등록 해줌으로서 다른클래스에서 의존성을 주입받아 사용할 수 있음.
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
