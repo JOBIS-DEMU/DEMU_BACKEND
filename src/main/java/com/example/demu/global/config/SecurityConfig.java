ac691c57e8ee3f18a84dbc6a202f82563ccef2e0
하이 <kkm07133@naver.com>
2024-10-28 22:36:04 +0900

    address add :: fix

--- src/main/java/com/example/demu/global/config/SecurityConfig.java
+++ src/main/java/com/example/demu/global/config/SecurityConfig.java
@@ -48,7 +48,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5713"));
+        config.setAllowedOrigins(List.of("http://localhost:5713", "http://3.37.219.136:8080", "http://localhost:3000"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowCredentials(true);
         config.setAllowedHeaders(List.of("*"));
