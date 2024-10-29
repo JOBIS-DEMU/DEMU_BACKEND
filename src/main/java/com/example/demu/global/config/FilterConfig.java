65fcd801ac20153c185918f8da853c934f0ceb63
하이 <kkm07133@naver.com>
2024-10-29 22:51:28 +0900

    cors :: fix

--- src/main/java/com/example/demu/global/config/FilterConfig.java
+++ src/main/java/com/example/demu/global/config/FilterConfig.java
@@ -10,13 +10,19 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+
 @RequiredArgsConstructor
 public class FilterConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
-    private final JwtProvider jwtProvider;
+    private final JwtProvider jwtTokenProvider;
     private final JwtReissueUtil jwtReissueUtil;
 
     @Override
-    public void configure(HttpSecurity builder) throws Exception {
-        builder.addFilterBefore(new JwtFilter(jwtProvider, jwtReissueUtil), UsernamePasswordAuthenticationFilter.class);
+    public void configure(HttpSecurity http) {
+
+        JwtFilter jwtTokenFilter = new JwtFilter(jwtTokenProvider, jwtReissueUtil);
+
+        http.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+
     }
+
 }
--- src/main/java/com/example/demu/global/config/SecurityConfig.java
+++ src/main/java/com/example/demu/global/config/SecurityConfig.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -12,56 +11,51 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
 import java.util.Arrays;
 import java.util.List;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
-public class SecurityConfig{
+public class SecurityConfig {
 
-
-    protected SecurityFilterChain configure(HttpSecurity httpSecurity) throws Exception {
-
-        return httpSecurity
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
                 .csrf()
                 .disable()
                 .cors()
                 .and()
                 .exceptionHandling()
-
                 .and()
                 .headers()
                 .frameOptions()
                 .disable()
-
                 .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-
                 .and()
-                .authorizeRequests()
+                .authorizeHttpRequests()
                 .anyRequest().permitAll()
-                .and().build();
+                .and()
+                .build();
     }
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-
-        configuration.setAllowedOrigins(List.of("*")); // 모든 도메인 허용
-        configuration.setAllowedMethods(Arrays.asList( "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE")); // HTTP 메서드 허용
+        configuration.setAllowedOrigins(List.of("*")); // 개발 환경에서만
+        configuration.setAllowedMethods(Arrays.asList("OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE"));
         configuration.setAllowCredentials(false);
-        configuration.addAllowedHeader("*"); // 모든 헤더 허용
+        configuration.addAllowedHeader("*");
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration); // 모든 경로에 대해 위에서 설정한 CORS 설정 적용
+        source.registerCorsConfiguration("/**", configuration);
         return source;
     }
-
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
 }
