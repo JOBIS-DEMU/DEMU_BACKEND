2b7b7c8fd1fb213ed284dd2558d70dd715bc478b
하이 <kkm07133@naver.com>
2024-10-29 18:48:09 +0900

    c

--- src/main/java/com/example/demu/global/config/WebConfig.java
+++ src/main/java/com/example/demu/global/config/WebConfig.java
@@ -1,14 +1,24 @@
 package com.example.demu.global.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//@Configuration
-//public class WebConfig implements WebMvcConfigurer {
-//    public static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH";
-//
-//    @Override
-//    public void addCorsMappings(final CorsRegistry registry) {
-//        registry.addMapping("/api/**")
-//                .allowedMethods(ALLOWED_METHOD_NAMES.split(",")));
-//    }
-//}
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOriginPatterns("*") // 안에 해당 주소를 넣어도 됨
+                        .allowedHeaders("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS" , "PATCH")
+                        .exposedHeaders("Authorization", "RefreshToken");
+                //.allowCredentials(true);
+            }
+        };
+    }
+}
