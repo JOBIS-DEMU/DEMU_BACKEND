c000689e1a33ea27feaa2ddaeb29128a76b3bb5a
하이 <kkm07133@naver.com>
2024-10-29 18:34:26 +0900

    c

---
+++ src/main/java/com/example/demu/global/config/WebConfig.java
@@ -0,0 +1,14 @@
+package com.example.demu.global.config;
+
+import org.springframework.context.annotation.Configuration;
+
+//@Configuration
+//public class WebConfig implements WebMvcConfigurer {
+//    public static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH";
+//
+//    @Override
+//    public void addCorsMappings(final CorsRegistry registry) {
+//        registry.addMapping("/api/**")
+//                .allowedMethods(ALLOWED_METHOD_NAMES.split(",")));
+//    }
+//}
