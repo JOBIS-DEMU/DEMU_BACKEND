1ecb96f2c19e8c7038f30f581f57ba0895339867
RyuJeaMin <kariel7673@gmail.com>
2024-11-03 14:02:37 +0900

    jj

--- src/main/java/com/example/demu/domain/auth/service/SignUpService.java
+++ src/main/java/com/example/demu/domain/auth/service/SignUpService.java
@@ -1,12 +1,15 @@
 package com.example.demu.domain.auth.service;
 
 import com.example.demu.domain.auth.controller.dto.SignUpRequest;
+import com.example.demu.domain.auth.exception.AccountIdAlreadyExistsException;
+import com.example.demu.domain.auth.exception.NicknameAlreadyExistsException;
 import com.example.demu.domain.auth.facade.UserFacade;
 import com.example.demu.domain.user.domain.User;
 import com.example.demu.domain.user.domain.repository.UserRepository;
 import com.example.demu.domain.user.domain.type.Grade;
 import com.example.demu.domain.user.domain.type.Major;
 import com.example.demu.global.security.TokenResponse;
+import com.example.demu.global.security.jwt.JwtReissueUtil;
 import com.example.demu.global.security.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -18,27 +21,32 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class SignUpService {
 
+    private final JwtReissueUtil jwtReissueUtil;
     private final JwtTokenProvider jwtTokenProvider;
     private final UserFacade userFacade;
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
     public TokenResponse execute(SignUpRequest signUpRequest) {
-    userFacade.CheckByaccountId(signUpRequest.getAccountId());
-    userFacade.CheckBynickname(signUpRequest.getNickname());
+        if(signUpRequest.getNickname().equals("aaaa")){
+            throw NicknameAlreadyExistsException.EXCEPTION;
+        }
+
+        userFacade.CheckByaccountId(signUpRequest.getAccountId());
+        userFacade.CheckBynickname(signUpRequest.getNickname());
 
 
-    userRepository.save(
-            User.builder()
-                    .accountId(signUpRequest.getAccountId())
-                    .nickname(signUpRequest.getNickname())
-                    .password(passwordEncoder.encode(signUpRequest.getPassword()))
-                    .grade(Grade.BRONZE)
-                    .major(Major.NONE)
-                    .point(0)
-                    .build()
-    );
+        userRepository.save(
+                User.builder()
+                        .accountId(signUpRequest.getAccountId())
+                        .nickname(signUpRequest.getNickname())
+                        .password(passwordEncoder.encode(signUpRequest.getPassword()))
+                        .grade(Grade.BRONZE)
+                        .major(Major.NONE)
+                        .point(0)
+                        .build()
+        );
 
-    return jwtTokenProvider.createToken(signUpRequest.getAccountId());
+        return jwtTokenProvider.createToken(signUpRequest.getAccountId());
     }
 }
